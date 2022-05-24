7a0260ca7fe220fe65d0cc1874a5134316b092c2
Alzoke <58400488+Alzoke@users.noreply.github.com>
2022-05-24 18:44:38 +0300

    Add files via upload

---
+++ DesktopFacebookApp/FacebookWinFormsApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+using FacebookWrapper;
+
+namespace BasicFacebookFeatures
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Clipboard.SetText("design.patterns20cc");
+            FacebookService.s_UseForamttedToStrings = true;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(FacebookAppFormFactory.CreateFacebookAppForm(eFormTypes.FormLogin));
+        }
+    }
+}
